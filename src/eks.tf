d8b7f9e23123d98e1393c2e1033365a2edc2b180
Ammar Tahir <ammartahir10@gmail.com>
2024-07-04 21:45:36 +0100

    Reduce name of worker group

--- src/eks.tf
+++ src/eks.tf
@@ -32,7 +32,7 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    exa-shared-worker-group = {
+    exa-shared-wg = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["m5.xlarge"]
